074f684d1265407b88855116ebf947b2c3bc386e
Владимир Иванов <vivanov879@ya.ru>
2015-06-08 06:52:07 +0300

    asdf

--- attention.lua
+++ attention.lua
@@ -7,9 +7,9 @@ require 'image'
 local model_utils=require 'model_utils'
 local mnist = require 'mnist'
 
-n = 12
-a = 28 
-b = 28 
+N = 12
+A = 28 
+B = 28 
 h_dec_n = 100
 x = nn.Identity()()
 h_dec = nn.Identity()()
@@ -22,3 +22,20 @@ delta = nn.Exp()(delta)
 gamma = nn.Exp()(gamma)
 gx = nn.AddConstant(1)(gx)
 gy = nn.AddConstant(1)(gy)
+gx = nn.MulConstant((A + 1) / 2)(gx)
+gy = nn.MulConstant((B + 1) / 2)(gy)
+delta = nn.MulConstant((math.max(A,B)-1)/(n-1))(delta)
+
+
+
+for a = 1, A do 
+  
+  
+  
+  
+end
+
+
+
+
+
