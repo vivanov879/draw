98098f4189ae3164aea241d98d954926f1049a46
Владимир Иванов <vivanov879@ya.ru>
2015-06-10 07:51:46 +0300

    asdf

--- attention1.lua
+++ attention1.lua
@@ -47,34 +47,30 @@ gx = nn.MulConstant((A + 1) / 2)(gx)
 gy = nn.MulConstant((A + 1) / 2)(gy)
 delta = nn.MulConstant((math.max(A,A)-1)/(N-1))(delta)
 
-ascending_x = nn.Identity()()
-ascending_y = nn.Identity()()
+ascending = nn.Identity()()
 
-filtered = {}
-
-for i = 1, N do
-  for j = 1, N do
-    mu_i = nn.CAddTable()({gx, nn.MulConstant(i - N/2 - 1/2)(delta)})
-    mu_j = nn.CAddTable()({gy, nn.MulConstant(j - N/2 - 1/2)(delta)})
-    mu_i = nn.MulConstant(-1)(mu_i)
-    mu_j = nn.MulConstant(-1)(mu_j)
-    d_i = nn.CAddTable()({mu_i, ascending_x})
-    d_j = nn.CAddTable()({mu_j, ascending_y})
-    d_i = nn.Power(2)(d_i)
-    d_j = nn.Power(2)(d_j)
-    exp_i = nn.CMulTable()({d_i, sigma})
-    exp_j = nn.CMulTable()({d_j, sigma})
-    exp_i = nn.Exp()(exp_i)
-    exp_j = nn.Exp()(exp_j)
-    filtered[#filtered + 1] = nn.View(n_data, 1)(nn.Sum(2)(nn.Sum(3)(nn.CMulTable()({exp_i, exp_j, x}))))
+function genr_filters(g)
+  filters = {}
+  for i = 1, N do
+      mu_i = nn.CAddTable()({g, nn.MulConstant(i - N/2 - 1/2)(delta)})
+      mu_i = nn.MulConstant(-1)(mu_i)
+      d_i = nn.CAddTable()({mu_i, ascending})
+      d_i = nn.Power(2)(d_i)
+      exp_i = nn.CMulTable()({d_i, sigma})
+      exp_i = nn.Exp()(exp_i)
+      exp_i = nn.View(n_data, 1, A)(exp_i)
+      filters[#filters + 1] = exp_i
   end
+  filterbank = nn.JoinTable(2)(filters)
+  return filterbank
 end
-    
-filtered_x = nn.JoinTable(2)(filtered)
-filtered_x = nn.Reshape(N, N)(filtered_x)
 
+filterbank_x = genr_filters(gx)
+filterbank_y = genr_filters(gy)
+patch = nn.MM()({filterbank_x, x})
+patch = nn.MM(false, true)({patch, filterbank_y})
 
-m = nn.gModule({x, gx_raw, gy_raw, delta_raw, sigma_raw, ascending_x, ascending_y}, {filtered_x})
+m = nn.gModule({x, gx_raw, gy_raw, delta_raw, sigma_raw, ascending}, {patch})
 
 
 trainset = mnist.traindataset()
@@ -87,24 +83,20 @@ for i = 1, n_data do
 end
 
 
-ascending_x = torch.zeros(n_data, A, A)
-ascending_y = torch.zeros(n_data, A, A)
+ascending = torch.zeros(n_data, A)
 for k = 1, n_data do
   for i = 1, A do 
-    for j = 1, A do 
-      ascending_x[k][i][j] = i
-      ascending_y[k][i][j] = j
-    end
+      ascending[k][i] = i
   end
 end
 
 
-gx = torch.zeros(n_data, A, A)
-gy = torch.zeros(n_data, A, A)
-sigma = torch.zeros(n_data, A, A)
-delta = torch.zeros(n_data, A, A)
+gx = torch.zeros(n_data, A)
+gy = torch.zeros(n_data, A)
+sigma = torch.zeros(n_data, A)
+delta = torch.zeros(n_data, A)
 
-z = m:forward({x, gx, gy, delta, sigma, ascending_x, ascending_y})
+z = m:forward({x, gx, gy, delta, sigma, ascending})
 
 print(x:gt(0.5))
 print(z:gt(0.5))
