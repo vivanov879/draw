2a34fb7854c6e1ce2cc49bfe24301220997c91fb
Владимир Иванов <vivanov879@ya.ru>
2015-06-11 11:30:02 +0300

    asdf

--- plot_results.lua
+++ plot_results.lua
@@ -10,11 +10,11 @@ nngraph.setDebug(true)
 
 x_prediction = torch.load('x_prediction')
 x = torch.zeros(#x_prediction, x_prediction[1]:size(2), x_prediction[1]:size(3)) 
-for t = 1, #x_prediction do 
-  x[{{t}, {}, {}}] = x_prediction[t][10]:gt(0.5)
-  
+for i = 1, x_prediction[1]:size(1) do
+  for t = 1, #x_prediction do 
+    x[{{t}, {}, {}}] = x_prediction[t][i]:gt(0.5)
+    
+  end
+  image.display(x)
 end
-
-
-image.display(x)
 
